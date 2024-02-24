7a38057cf675e86f1ab0f7f3ce07b2ced2adce20
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-24 12:50:02 +0300

    fetch all drinks from database and return a json

--- drinks/views.py
+++ drinks/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Drink
+from .serializers import DrinkSerializer
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def drink_list(request):
+   if request.method == 'GET':
+       # get all drinks
+        drinks = Drink.objects.all()
+
+        #serialize the drinks
+        serializer = DrinkSerializer(drinks, many=True)
+
+        #return the serialized 
+        return JsonResponse(serializer.data, safe=False)
+   
+   
